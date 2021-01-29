48abf401030497eceba6b8be6a188d791de96110
Jimmie Harkins <ghana796@gmail.com>
2021-01-29 01:23:34 -0500

    Added tooltips for infused items

--- src/main/java/ghana7/trinketeering/item/Infuseable.java
+++ src/main/java/ghana7/trinketeering/item/Infuseable.java
@@ -1,33 +1,25 @@
 package ghana7.trinketeering.item;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.text.*;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
 
 public abstract class Infuseable extends Item {
     public Infuseable(Properties properties) {
         super(properties);
     }
-
-    public IItemHandler getInfusionInventory(ItemStack stack) {
-        ItemStackHandler stackHandler = createInfusionHandler();
-        stackHandler.deserializeNBT(stack.getOrCreateTag().getCompound("InfusionInventory"));
-        return stackHandler;
-    }
-    public void saveInfusionInventory(ItemStack stack, IItemHandler itemHandler) {
-        if(itemHandler instanceof ItemStackHandler) {
-            stack.getOrCreateTag().put("InfusionInventory", ((ItemStackHandler)itemHandler).serializeNBT());
-        }
-    }
-    public static boolean canInfuseWith(Item item)
-    {
-        return item == Items.SNOWBALL || item == Items.BLAZE_POWDER;
-    }
-
     private ItemStackHandler createInfusionHandler() {
         return new ItemStackHandler(4) {
             @Override
@@ -59,4 +51,33 @@ public abstract class Infuseable extends Item {
             }
         };
     }
+    public IItemHandler getInfusionInventory(ItemStack stack) {
+        ItemStackHandler stackHandler = createInfusionHandler();
+        stackHandler.deserializeNBT(stack.getOrCreateTag().getCompound("InfusionInventory"));
+        return stackHandler;
+    }
+    public void saveInfusionInventory(ItemStack stack, IItemHandler itemHandler) {
+        if(itemHandler instanceof ItemStackHandler) {
+            stack.getOrCreateTag().put("InfusionInventory", ((ItemStackHandler)itemHandler).serializeNBT());
+        }
+    }
+    public static boolean canInfuseWith(Item item)
+    {
+        return item == Items.SNOWBALL || item == Items.BLAZE_POWDER;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        CompoundNBT nbt = stack.getOrCreateTag();
+        if(nbt.contains("InfusionInventory")) {
+            IItemHandler stackHandler = getInfusionInventory(stack);
+            for(int i = 0; i < stackHandler.getSlots(); i++) {
+                if(!stackHandler.getStackInSlot(i).isEmpty()) {
+                    tooltip.add(new TranslationTextComponent(stackHandler.getStackInSlot(i).getTranslationKey()));
+                }
+            }
+        }
+    }
+
 }
